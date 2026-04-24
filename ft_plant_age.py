622f7339264ca7d3023937abb6c460bed8b8ad80
Raad <raad@example.com>
2026-04-24 23:42:38 +0300

    python

---
+++ ft_plant_age.py
@@ -0,0 +1,8 @@
+def ft_plant_age():
+    days = int(input("Enter plant age in days: "))
+    if days > 60:
+        print("plant is ready to harvest")
+    else:
+        print("plant need more time to grow")
+ft_plant_age()
+
